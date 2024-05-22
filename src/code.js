f6fd3a8605c4df05149f6f3986f262bfefbaef54
Roger Gomà Roca <rogergoma@bihotz-studio.com>
2024-05-22 12:12:20 +0200

    DE columnas a filas

--- src/code.js
+++ src/code.js
@@ -2,7 +2,7 @@ figma.showUI(__html__, { width: 500, height: 500 });
 
 figma.ui.onmessage = async (msg) => {
   if (msg.type === "create-frames") {
-    const { data, templateName } = msg;
+    const { data, campaignName, templateName } = msg;
 
     // Buscar el frame llamado según el nombre del template proporcionado por el usuario
     const templateFrame = figma.currentPage.findOne(
@@ -17,64 +17,21 @@ figma.ui.onmessage = async (msg) => {
     // Iterar sobre cada idioma y crear un frame debajo del anterior
     let offsetY = templateFrame.y + templateFrame.height + 50; // Espacio vertical entre frames
 
-    for (const rowData of data) {
-      const {
-        language,
-        title1,
-        title2,
-        description1,
-        description2,
-        buttonText,
-        campaign,
-      } = rowData;
-
-      // Clonar el frame "Template"
+    for (const [language, textData] of Object.entries(data)) {
       const newFrame = templateFrame.clone();
       newFrame.x = templateFrame.x;
       newFrame.y = offsetY;
-      newFrame.name = `${campaign}_${language}`; // Configurar el nombre del frame con el idioma y campaña
+      newFrame.name = `${campaignName}_${language}`; // Configurar el nombre del frame con el idioma y campaña
 
       // Buscar y reemplazar los textos dentro del nuevo frame
-      const titleNode1 = newFrame.findOne(
-        (node) => node.name === "Title1" && node.type === "TEXT"
-      );
-      const titleNode2 = newFrame.findOne(
-        (node) => node.name === "Title2" && node.type === "TEXT"
-      );
-      const descriptionNode1 = newFrame.findOne(
-        (node) => node.name === "Description1" && node.type === "TEXT"
-      );
-      const descriptionNode2 = newFrame.findOne(
-        (node) => node.name === "Description2" && node.type === "TEXT"
-      );
-      const buttonNode = newFrame.findOne(
-        (node) => node.name === "Button" && node.type === "TEXT"
-      );
-
-      // Asignar textos a los nodos
-      if (titleNode1 && title1) {
-        await figma.loadFontAsync(titleNode1.fontName);
-        titleNode1.characters = title1;
-      }
-
-      if (titleNode2 && title2) {
-        await figma.loadFontAsync(titleNode2.fontName);
-        titleNode2.characters = title2;
-      }
-
-      if (descriptionNode1 && description1) {
-        await figma.loadFontAsync(descriptionNode1.fontName);
-        descriptionNode1.characters = description1;
-      }
-
-      if (descriptionNode2 && description2) {
-        await figma.loadFontAsync(descriptionNode2.fontName);
-        descriptionNode2.characters = description2;
-      }
-
-      if (buttonNode && buttonText) {
-        await figma.loadFontAsync(buttonNode.fontName);
-        buttonNode.characters = buttonText;
+      for (const [textType, textValue] of Object.entries(textData)) {
+        const node = newFrame.findOne(
+          (n) => n.name === textType && n.type === "TEXT"
+        );
+        if (node && textValue) {
+          await figma.loadFontAsync(node.fontName);
+          node.characters = textValue;
+        }
       }
 
       // Actualizar el offset para el siguiente frame
